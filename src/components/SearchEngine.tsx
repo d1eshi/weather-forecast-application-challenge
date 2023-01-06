54fd496a4915d12c20caafa83d8147092c5b7c15
Diego Silva <silva.diegof03@gmail.com>
2023-01-06 18:38:35 -0300

    [add] loader

--- src/components/SearchEngine.tsx
+++ src/components/SearchEngine.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Flex, Input, Select, Icon, Text } from '@chakra-ui/react'
+import { Box, Flex, Input, Select, Icon, Text, Button } from '@chakra-ui/react'
 import { apiWeather } from '../../api/axios'
 import { useAppDispatch, useAppSelector } from '../../redux/hooks'
 import { fillWeatherRes, selectWeatherRes } from '../../redux/slices/weatherResponseSlice'
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import { MapPinIcon } from '@heroicons/react/24/outline'
 import { getFormattedWeatherData, getWeatherData } from '../../services/weatherService'
+import { Circles } from 'react-loader-spinner'
 
 const API_KEY = import.meta.env.VITE_API_KEY
 export const SearchEngine = () => {
@@ -14,6 +15,7 @@ export const SearchEngine = () => {
   const [units, setUnits] = React.useState('metric')
   const [typeOfSearch, setTypeOfSearch] = React.useState<string>('name')
   const [weather, setWeather] = React.useState(null)
+  const [isLoading, setIsLoading] = React.useState(false)
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
 
@@ -26,12 +28,12 @@ export const SearchEngine = () => {
 
     let url: string = ''
     let data
+    setIsLoading(true)
 
     switch (typeOfSearch) {
       case 'name':
         data = await getFormattedWeatherData({ q: location.trim(), units })
         console.log(data)
-
         dispatch(fillWeatherRes(data))
 
         break
@@ -51,6 +53,8 @@ export const SearchEngine = () => {
       default:
         break
     }
+    if (!data) return
+    setIsLoading(false)
     navigate(`/forecast?${location}`)
   }
 
@@ -71,6 +75,22 @@ export const SearchEngine = () => {
     <div>
       <Flex align='center' gap={3} as='form' onSubmit={handleSubmit}>
         {/* <FormLabel htmlFor='location'>Location:</FormLabel> */}
+        {isLoading && (
+          <Box
+            display='flex'
+            alignItems='center'
+            justifyContent='center'
+            bgColor='#eeeeeec9'
+            position='absolute'
+            sx={{ top: 0, left: 0 }}
+            zIndex={1}
+            w='100%'
+            h='100%'
+          >
+            <Circles color='#00BFFF' height={100} width={100} visible={isLoading} />
+          </Box>
+        )}
+
         <Flex gap={2} w={['61%', '71%']}>
           <Input
             autoFocus
@@ -92,16 +112,48 @@ export const SearchEngine = () => {
           </Select>
         </Flex>
         <Flex w='13%' gap={2} color='white'>
-          <Icon cursor='pointer' transition='ease-out ' _hover={{ boxSize: 7 }} as={MagnifyingGlassIcon} boxSize={6} />
-          <Icon cursor='pointer' transition='ease-out ' _hover={{ boxSize: 7 }} as={MapPinIcon} boxSize={6} />
+          <Icon
+            maxW='1.5em'
+            cursor='pointer'
+            transition='ease-out'
+            _hover={{ color: '#9ae1e1f0' }}
+            as={MagnifyingGlassIcon}
+            boxSize={6}
+          />
+          <Icon
+            maxW='1.5em'
+            _hover={{ color: '#9ae1e1f0' }}
+            cursor='pointer'
+            transition='ease-out'
+            as={MapPinIcon}
+            boxSize={6}
+          />
         </Flex>
 
         <Flex fontSize='1.5rem' color='white'>
-          <button name='metric'>°C</button>
+          <Button
+            bg='transparent'
+            fontWeight='300'
+            fontSize='2rem'
+            maxW='1em'
+            name='metric'
+            _hover={{ fontSize: '2.2rem' }}
+          >
+            °C
+          </Button>
           <Text as='p' mx={1}>
             |
           </Text>
-          <button name='imperial'>°F</button>
+          <Button
+            bg='transparent'
+            fontWeight='300'
+            fontSize='2rem'
+            maxW='1em'
+            name='imperial'
+            _hover={{ fontSize: '2.2rem' }}
+          >
+            °F
+          </Button>
         </Flex>
         {/* <Icon as={MapPinIcon} boxSize={6} /> */}
       </Flex>
