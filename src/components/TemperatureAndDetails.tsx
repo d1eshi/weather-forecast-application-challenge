2c6a010d763c83e888716ced5eb55d61a11e223b
Diego Silva <silva.diegof03@gmail.com>
2023-01-06 13:11:29 -0300

    [add] temperature and details

---
+++ src/components/TemperatureAndDetails.tsx
@@ -0,0 +1,14 @@
+import { Flex, Text } from '@chakra-ui/react'
+import React from 'react'
+
+export const TemperatureAndDetails = () => {
+  return (
+    <>
+      <Flex align='center' justify='center' py={4}>
+        <Text fontSize='2xl' color='cyan.300'>
+          Cloudy or whatever
+        </Text>
+      </Flex>
+    </>
+  )
+}
