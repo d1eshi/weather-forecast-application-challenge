aa8746df86d4346e114796d5f378251005ad1967
Diego Silva <silva.diegof03@gmail.com>
2023-01-06 06:37:40 -0300

    [add] navbar for all pages and some styles from container

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
+import { Box } from '@chakra-ui/react'
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Navbar } from './components/Navbar'
 import { Home } from './pages/Home'
 import Search from './pages/Search'
 
 function App() {
   return (
-    <Routes>
-      <Route index path='/' element={<Home />} />
-      <Route path='/search' element={<Search />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Box mt={3} mx='auto' maxW='50%'>
+        <Routes>
+          <Route index path='/' element={<Home />} />
+          <Route path='/search' element={<Search />} />
+        </Routes>
+      </Box>
+    </>
   )
 }
 
